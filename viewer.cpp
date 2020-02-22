4fdd855053204b2c7666dae3b8383bb5f4ba20f1
Gleb Mazovetskiy <glex.spb@gmail.com>
2020-02-22 12:09:07 +0900

    Support horizontal DPI-scaling
    Useful when developing on a high DPI display

--- viewer.cpp
+++ viewer.cpp
@@ -47,9 +47,9 @@ CViewer::CViewer(const std::string &p_fileName):
     m_image(nullptr)
 {
     // Create background image
-    m_background = SDL_utils::createImage(SCREEN_WIDTH, SCREEN_HEIGHT * PPU_Y, SDL_MapRGB(Globals::g_screen->format, COLOR_BG_1));
+    m_background = SDL_utils::createImage(SCREEN_WIDTH * PPU_X, SCREEN_HEIGHT * PPU_Y, SDL_MapRGB(Globals::g_screen->format, COLOR_BG_1));
     {
-        SDL_Rect l_rect = {0, 0, SCREEN_WIDTH, Y_LIST * PPU_Y};
+        SDL_Rect l_rect = {0, 0, SCREEN_WIDTH * PPU_X, Y_LIST * PPU_Y};
         SDL_FillRect(m_background, &l_rect, SDL_MapRGB(m_background->format, COLOR_BORDER));
     }
     // Print title
@@ -85,7 +85,11 @@ CViewer::CViewer(const std::string &p_fileName):
         int i = 0;
         for (int y = Y_LIST; y < SCREEN_HEIGHT; y += kTransparentBgRectSize, ++i) {
             for (int x = 0; x < SCREEN_WIDTH; x += kTransparentBgRectSize, ++i) {
-                SDL_Rect rect = {x, y * PPU_Y, kTransparentBgRectSize, kTransparentBgRectSize * PPU_Y};
+                SDL_Rect rect = {
+                    static_cast<decltype(SDL_Rect().x)>(x * PPU_X),
+                    static_cast<decltype(SDL_Rect().y)>(y * PPU_Y),
+                    static_cast<decltype(SDL_Rect().w)>(kTransparentBgRectSize * PPU_X),
+                    static_cast<decltype(SDL_Rect().h)>(kTransparentBgRectSize * PPU_Y)};
                 SDL_FillRect(m_background, &rect, colors[i % 2]);
             }
         }
@@ -97,7 +101,7 @@ CViewer::CViewer(const std::string &p_fileName):
         // Init clip rect
         m_clip.x = 0;
         m_clip.y = 0;
-        m_clip.w = SCREEN_WIDTH - 2 * VIEWER_MARGIN;
+        m_clip.w = (SCREEN_WIDTH - 2 * VIEWER_MARGIN) * PPU_X;
 
         std::ifstream l_file(m_fileName.c_str());
 
@@ -133,7 +137,7 @@ void CViewer::render(const bool p_focus) const
     SDL_utils::applySurface(0, 0, m_background, Globals::g_screen);
     if (m_mode == IMAGE)
     {
-        SDL_utils::applySurface((SCREEN_WIDTH - m_image->w) / 2, Y_LIST + (SCREEN_HEIGHT - Y_LIST - m_image->h / PPU_Y) / 2, m_image, Globals::g_screen);
+        SDL_utils::applySurface((SCREEN_WIDTH - m_image->w / PPU_X) / 2, Y_LIST + (SCREEN_HEIGHT - Y_LIST - m_image->h / PPU_Y) / 2, m_image, Globals::g_screen);
     }
     else if (m_mode == TEXT)
     {
@@ -262,8 +266,8 @@ bool CViewer::moveLeft(void)
     bool l_ret(false);
     if (m_clip.x > 0)
     {
-        if (m_clip.x > VIEWER_X_STEP)
-            m_clip.x -= VIEWER_X_STEP;
+        if (m_clip.x > VIEWER_X_STEP * PPU_X)
+            m_clip.x -= VIEWER_X_STEP * PPU_X;
         else
             m_clip.x = 0;
         l_ret = true;
@@ -273,6 +277,6 @@ bool CViewer::moveLeft(void)
 
 bool CViewer::moveRight(void)
 {
-    m_clip.x += VIEWER_X_STEP;
+    m_clip.x += VIEWER_X_STEP * PPU_X;
     return true;
 }
