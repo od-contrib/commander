b168c52d765340bc6d13386e543908ac89e847f5
Gleb Mazovetskiy <glex.spb@gmail.com>
2020-02-22 12:20:40 +0900

    Fix splitter/cursor positioning

--- resourceManager.cpp
+++ resourceManager.cpp
@@ -40,8 +40,8 @@ CResourceManager::CResourceManager(void) :
     m_surfaces[T_SURFACE_FILE_INSTALLABLE_PACKAGE] = LoadIcon(RES_DIR "file-ipk.png");
     m_surfaces[T_SURFACE_FILE_PACKAGE] = LoadIcon(RES_DIR "file-opk.png");
     m_surfaces[T_SURFACE_UP] = LoadIcon(RES_DIR "up.png");
-    m_surfaces[T_SURFACE_CURSOR1] = SDL_utils::createImage(159 * PPU_X, LINE_HEIGHT * PPU_Y, SDL_MapRGB(Globals::g_screen->format, COLOR_CURSOR_1));
-    m_surfaces[T_SURFACE_CURSOR2] = SDL_utils::createImage(159 * PPU_X, LINE_HEIGHT * PPU_Y, SDL_MapRGB(Globals::g_screen->format, COLOR_CURSOR_2));
+    m_surfaces[T_SURFACE_CURSOR1] = SDL_utils::createImage(SCREEN_WIDTH / 2 * PPU_X, LINE_HEIGHT * PPU_Y, SDL_MapRGB(Globals::g_screen->format, COLOR_CURSOR_1));
+    m_surfaces[T_SURFACE_CURSOR2] = SDL_utils::createImage(SCREEN_WIDTH / 2 * PPU_X, LINE_HEIGHT * PPU_Y, SDL_MapRGB(Globals::g_screen->format, COLOR_CURSOR_2));
     // Load font
     m_font = SDL_utils::loadFont(RES_DIR "wy_scorpio.ttf", 8);
 }
